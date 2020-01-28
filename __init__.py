27cf722fcb7fc540a278de6a0494db5d80e760ce
KarlKarindi <karl_erik97@hotmail.com>
2020-01-29 00:29:21 +0200

    #5 db setup halfway done

--- __init__.py
+++ __init__.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from estnltk import Vabamorf
 from program_utils import *
 import csv
 
 # Initialize vabamorf singleton
 vabamorf = Vabamorf.instance()
+
+POSTGRES_URL = ""
+POSTGRES_USER = "postgres"
+POSTGRES_PW = "mandariin"
+POSTGRES_DB = "analyzer"
+
+DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
+    user=POSTGRES_USER, pw=POSTGRES_PW, url=POSTGRES_URL, db=POSTGRES_DB)
+
 
 # Create csv file for lemmas #
 create_csv_file_for_lemmas(
@@ -14,6 +24,12 @@ create_csv_file_for_lemmas(
 def create_app():
     from . import routes, services, model
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db = SQLAlchemy(app)
+
     routes.init_app(app)
+
     print("App update/creation successful.\n\n\n")
     return app
