76f84f414bc77fc7a40a3e3c2889f2a0a891c8a3
KarlKarindi <karl_erik97@hotmail.com>
2020-02-04 15:07:35 +0200

    #1 fixed bug creating too many ClauseSegmenter objects

--- ThesisAnalyzer/Services/Analysis/Style/Config/config.py
+++ ThesisAnalyzer/Services/Analysis/Style/Config/config.py
@@ -1,4 +1,4 @@
 # Config file for style analysis
 
-MAX_CLAUSE_AMOUNT = 6
+MAX_CLAUSE_AMOUNT = 5
 OVERUSED_MULTIPLIER = 5
--- ThesisAnalyzer/Services/style_main.py
+++ ThesisAnalyzer/Services/style_main.py
@@ -1,6 +1,4 @@
-from ThesisAnalyzer.Services.Analysis.Style import overused_word_analyzer
-from ThesisAnalyzer.Services.Analysis.Style import clause_analyzer
-from ThesisAnalyzer.Services.Analysis.Style import tag_analyzer
+from ThesisAnalyzer.Services.Analysis.Style import overused_word_analyzer, clause_analyzer, tag_analyzer
 from ThesisAnalyzer.Models.Feedback import StyleFeedback
 from ThesisAnalyzer.Services.utils import json_to_text
 
@@ -12,12 +10,12 @@ def analyze(request):
     text = json_to_text(request)
 
     # Word repeat analysis
-    overused_word_analyzer.analyze_overused_words(text)
+    #overused_word_analyzer.analyze_overused_words(text)
 
     # Clause analysis
     clause_analyzer.analyze_clauses(text)
 
     # Tag analysis
-    tag_analyzer.analyze_adverbs(text)
+    # tag_analyzer.analyze_adverbs(text)
 
     return jsonify(length=feedback.length)
