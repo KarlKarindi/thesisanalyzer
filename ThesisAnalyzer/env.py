e505c2c1e6d52c463caa248393b6ff29bffc8ead
Karl Erik Karindi <karl.karindi@gmail.com>
2023-01-21 02:14:41 +0200

    Fix minor issues with docker setup

---
+++ ThesisAnalyzer/env.py
@@ -0,0 +1,17 @@
+import os
+import platform
+import pathlib
+
+
+def get_vislcg3_path():
+    """ Returns the path of the vislcg3 binaries depending on OS """
+
+    if platform.system() == "Windows":
+        return os.getcwd() + "\\ThesisAnalyzer\\lib\\vislcg_windows\\vislcg3.exe"
+    else:
+        # Assume the OS is Ubuntu, use Ubuntu binaries
+        return str(pathlib.Path(__file__).parent) + "/lib/vislcg_ubuntu/cg3"
+
+
+INDEX_PAGE = "/"
+LOG_TO_DATABASE = False
