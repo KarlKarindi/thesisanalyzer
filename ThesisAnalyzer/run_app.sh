5d5c8d023d09ec9134724c489bfc21ccbb7c95d1
Karl Erik Karindi <karl.karindi@gmail.com>
2023-03-07 02:58:53 +0200

    Increase number of workers

--- ThesisAnalyzer/run_app.sh
+++ ThesisAnalyzer/run_app.sh
@@ -2,4 +2,4 @@
 echo "Activating the environment!"
 ~/miniconda/bin/conda init
 echo "Running server..."
-~/miniconda/bin/conda run -n thesis-env-3.6 gunicorn ThesisAnalyzer.wsgi:app
+~/miniconda/bin/conda run -n thesis-env-3.6 gunicorn ThesisAnalyzer.wsgi:app --name thesisanalyzer --workers 10 --timeout 900
