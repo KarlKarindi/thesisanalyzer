4c95bd636c0005b2ee29b02c3cf4a76114dc9cf7
Karl Karindi <karl.karindi@gmail.com>
2023-03-12 02:13:38 +0200

    Simplify dockerfile

--- ThesisAnalyzer/run_app.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Activating the environment!"
-~/miniconda/bin/conda init
-echo "Running server..."
-~/miniconda/bin/conda run -n thesis-env-3.6 gunicorn ThesisAnalyzer.wsgi:app --name thesisanalyzer --workers 10 --timeout 900
