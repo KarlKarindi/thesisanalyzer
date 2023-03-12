d6e48bddaf00410f6cde326d151a5f342857b487
Ubuntu <ubuntu@thesisanalyzer.cloud.ut.ee>
2023-03-12 15:54:37 +0200

    Add logging and missing nltk dep

--- ThesisAnalyzer/__init__.py
+++ ThesisAnalyzer/__init__.py
@@ -1,6 +1,11 @@
+import logging
+import nltk
+from logging.handlers import RotatingFileHandler
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from .db import DB_USER, DB_PASS
+
+nltk.download('punkt')
 
 DB_ADDRESS = "localhost"
 DB_PORT = "5432"
@@ -23,4 +28,15 @@ DB_URL = "postgres://"+DB_USER+":"+DB_PASS+"@thesisanalyzer-db:5432/analyzer"
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
 # silence the deprecation warning
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# configure logging
+# Set up a file handler for Flask's default logger
+file_handler = RotatingFileHandler('logs/flask.log', maxBytes=10240, backupCount=10)
+file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+file_handler.setLevel(logging.INFO)
+app.logger.addHandler(file_handler)
+
+# Set the log level to INFO for Flask's default logger
+app.logger.setLevel(logging.INFO)
+
 app.run(host='0.0.0.0', port=5000)
